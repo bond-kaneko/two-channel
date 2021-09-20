7fbab5eed14a293fa6fb973ffa8294e97d933953
junki.kaneko <junki.kaneko@linecorp.com>
2021-09-20 11:41:01 +0900

    routing

--- src/index.js
+++ src/index.js
@@ -1,17 +1,66 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+
+import {
+    BrowserRouter as Router,
+    Switch,
+    Route,
+    Link
+} from "react-router-dom";
+
+class Board extends React.Component {
+
+    render() {
+        return (
+            <h1>掲示板一覧</h1>
+        );
+    }
+}
+
+
+class Thread extends React.Component {
+    render() {
+        return (
+            <h1>スレ一覧</h1>
+        );
+    }
+}
+
+class ThreadDetail extends React.Component {
+    render() {
+        return (
+            <h1>スレ詳細</h1>
+        )
+    }
+}
+
+class App extends React.Component {
+    render() {
+        return (
+            <Router>
+                <Switch>
+                    <Route path="/board">
+                        <Board />
+                    </Route>
+                    <Route path="/thread/detail">
+                        <ThreadDetail />
+                    </Route>
+                    <Route path="/thread">
+                        <Thread />
+                    </Route>
+                    <Route path="/">
+                        <Board />
+                    </Route>
+                </Switch>
+            </Router>
+        );
+    }
+}
+
+// ========================================
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
+    <App />,
+    document.getElementById('root')
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
