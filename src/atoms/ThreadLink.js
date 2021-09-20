e13dd7fbc02dc21fc7d8b0b8fce8f2e9eca45b06
junki.kaneko <junki.kaneko@linecorp.com>
2021-09-20 14:40:08 +0900

    スレ一覧

---
+++ src/atoms/ThreadLink.js
@@ -0,0 +1,9 @@
+import React from "react";
+
+const ThreadLink = (props) => {
+    return (
+        <><a href={props.href}>{props.title}</a><span>/</span></>
+    );
+}
+
+export default ThreadLink;
--- src/pages/BoardIndex.js
+++ src/pages/BoardIndex.js
@@ -1,8 +1,20 @@
 import React from "react"
+import ThreadLink from "../atoms/ThreadLink";
 
 const BoardIndex = () => {
+    // TODO スレ一覧の取得
+    const threads = [
+        { id: 1, title: "スレ1" },
+        { id: 2, title: "スレ2" }
+    ];
+
     return (
-        <h1>掲示板一覧</h1>
+        <>
+            <h1>掲示板一覧</h1>
+            {threads.map((thread) =>
+                <ThreadLink title={thread.title} href="#" />
+            )}
+        </>
     );
 }
 
